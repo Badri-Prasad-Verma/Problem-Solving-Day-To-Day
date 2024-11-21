79008547244a749ff5354851c0f2cb844a1165af
Badri-Prasad-Verma <jmnaprasad261@gmail.com>
2024-11-21 23:27:55 +0530

    Sort the given array in assending order

---
+++ java_project/src/Day1/SortArrayInAssen.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class SortArrayInAssen {
+	public static void main(String[] args) {
+		int[] arr= {66,44,11,77,22,99,33,88,55};
+		
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = 0; j < arr.length-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
