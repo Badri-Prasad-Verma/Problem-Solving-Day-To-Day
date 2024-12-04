f7a25aa01aed06d084ac22fb64b24b1fe8c9025d
Badri-Prasad-Verma <jmnaprasad261@gmail.com>
2024-12-04 23:18:49 +0530

    Find the average of given array

---
+++ java_project/src/Day14/AverageOfGivenArray.java
@@ -0,0 +1,28 @@
+package Day14;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class AverageOfGivenArray {
+
+	public static void main(String[] args) {
+	
+		int[] arr = {10, 76, 34, 86, 65, 89, 23, 45};
+		
+		//Approach 1 
+		int sum =0;
+		for (int num : arr) {
+			sum+=num;
+		}
+		
+		double average = (double) sum/ arr.length;
+		System.out.println("Average of all numbers is : "+average);
+		
+		//Approach 2 using java 8
+		double average1 = Arrays.stream(arr).average().orElse(0.0);
+		System.out.println("Average of all numbers is : "+average1);
+		
+		
+	}
+	
+}
