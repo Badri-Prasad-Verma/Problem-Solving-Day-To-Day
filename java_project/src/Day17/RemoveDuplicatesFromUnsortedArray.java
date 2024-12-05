3103f4359130a30cbb7c83901331de3fd0ddecd8
Badri-Prasad-Verma <jmnaprasad261@gmail.com>
2024-12-05 23:00:43 +0530

    Remove the duplicates from given unnsorted array

--- java_project/src/Day17/RemoveDuplicatesFromUnsortedArray.java
+++ java_project/src/Day17/RemoveDuplicatesFromUnsortedArray.java
@@ -2,33 +2,35 @@ package Day17;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.stream.IntStream;
 
 public class RemoveDuplicatesFromUnsortedArray {
 
 	public static void main(String[] args) {
 
+		//Approach 1 using loop and recursion/
 		 int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
 		 
-		 int[] uniqueArray = removeDuplicate(arr);
-	     System.out.println("Array after removing duplicates: " + Arrays.toString(uniqueArray));
+		 HashSet<Integer> hashSet = new HashSet<Integer>();
+			
+			for (Integer integer : arr) {
+				hashSet.add(integer);
+			}
+			
+			int[] uniqueArray =new int[hashSet.size()];
+			int i=0;
+			
+			for (int num : hashSet) {
+				uniqueArray[i++] = num;
+			}
+			
+	        System.out.println("Array after removing duplicates: " + Arrays.toString(uniqueArray));
 
+	        //Approach 2 using java8
+	        
+	        int[] array = IntStream.of(arr).distinct().toArray();
+	        System.out.println(Arrays.toString(array));
 		 
 	}
-
-	private static int[] removeDuplicate(int[] arr) {
-		HashSet<Integer> hashSet = new HashSet<Integer>();
-		
-		for (Integer integer : arr) {
-			hashSet.add(integer);
-		}
-		
-		int[] unique =new int[hashSet.size()];
-		int i=0;
-		
-		for (int num : hashSet) {
-			unique[i++] = num;
-		}
-		
-		return unique;
-	}
+	
 }
